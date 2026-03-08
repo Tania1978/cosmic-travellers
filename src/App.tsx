3258bff5cd310cc0e45088ff13f099f7b612929b
Tania1978 <taniakarageorgi@gmail.com>
2026-03-08 08:50:16 +0200

    div to test on click

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Header from "./components/Header";
 import AppRoutes from "./AppRoutes";
 import Footer from "./components/Footer";
 import { useAuth } from "./auth/authContext";
-import { AppAudio } from "./components/AppAudio";
+
 
 
 export default function App() {
@@ -13,7 +13,6 @@ export default function App() {
   return (
     <>
       <Header />
-      <AppAudio/>
       <AppRoutes />
       <Footer />
     </>
